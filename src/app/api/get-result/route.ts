8f801a4d14bbdb6c1e611039ef21eabc71ca9be3
hardik <hardiksadhu472@gmail.com>
2025-03-05 11:22:22 +0000

    added frontend

--- src/app/api/get-result/route.ts
+++ src/app/api/get-result/route.ts
@@ -39,3 +39,20 @@ export async function POST(req: NextRequest) {
     });
   }
 }
+
+export async function OPTIONS(request: Request) {
+  const allowedOrigin = request.headers.get("origin");
+  console.log(allowedOrigin)
+  const response = new NextResponse(null, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin":"*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+      "Access-Control-Max-Age": "86400",
+    },
+  });
+
+  return response;
+}
